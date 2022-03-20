c41c88312aaf8df3d1ffac10c149d58c73a820e8
Cesar dos Santos de Almeida <cesarsantosss499@gmail.com>
2022-03-20 14:51:22 -0300

    Criação e Correção: Desenvolvimento da home e alteração na responsividade do Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 // Importação de Components
 import Header from "./components/Header/Header"
+import Home from "./components/Home/Home"
 
 function App() {
   return (
-    <Header></Header>
+    <>
+      <Home>
+        <Header></Header>
+      </Home>
+    </>
   )
 }
 
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Home.css"
+
+// IMG
+import Illustration from "../../img/Illustration.svg"
+import Lupa from "../../img/lupa.png"
+
+function Home(props) {
+    
+    return (
+        <section id="home" >
+            { props.children}
+            <div className="cell-total container">
+                <div className="cell-quite">
+                    <div>
+                        <h1>Ready for Trying a new recipe?</h1>
+                        <div className="cell-field">
+                            <input type="search" id="search" placeholder="Search healthy recipes"/>
+                            <button><img src={Lupa} alt="" /></button>
+                        </div>
+                    </div>
+                </div>
+                <div className="cell-quite">
+                    {/* Coluna falsa */}
+                </div>
+            </div>
+            <img id="illustration-home" src={Illustration} alt="" />
+        </section>
+    )
+}
+
+export default Home
