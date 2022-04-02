32624c7c46acc2e88d2444744d5197b3157238b3
Cesar dos Santos de Almeida <cesarsantosss499@gmail.com>
2022-04-02 12:28:46 -0300

    Correção: alteração na estrutura  do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
 // Importação de Components
-import Header from "./components/Header/Header"
-import Home from "./components/Home/Home"
-import Revenue from "./components/Revenue/Revenue"
-import Service from "./components/Service/Service"
-import Blog from "./components/Blog/Blog"
-import Join from "./components/Join/Join"
-import Rodape from "./components/Rodape/Rodape"
+import Header from "./components/Header"
+import Home from "./components/Home"
+import Revenue from "./components/Revenue"
+import Service from "./components/Service"
+import Blog from "./components/Blog"
+import Join from "./components/Join"
+import Rodape from "./components/Footer"
 
 function App() {
   return (
     <>
       <Home>
-        <Header></Header>
+        <Header/>
       </Home>
-      <Revenue></Revenue>
-      <Service></Service>
-      <Blog></Blog>
-      <Join></Join>
-      <Rodape></Rodape>
+      <Revenue/>
+      <Service/>
+      <Blog/>
+      <Join/>
+      <Rodape/>
     </>
   )
 }
